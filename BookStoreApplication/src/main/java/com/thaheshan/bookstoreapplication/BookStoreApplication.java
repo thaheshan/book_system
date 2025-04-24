d76da4eb04d2466cf8e9233eabc4111b7d146ca6
thaheshan suresh <166691277+thaheshan@users.noreply.github.com>
2025-04-24 23:40:34 +0530

    created main class
    created main entry point classes to manage those every classes for the book system

---
+++ BookStoreApplication/src/main/java/com/thaheshan/bookstoreapplication/BookStoreApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.thaheshan.bookstoreapplication;
+
+/**
+ *
+ * @author Thahe
+ */
+public class BookStoreApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
